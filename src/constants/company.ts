5f564f0638a892929a7a5a4c0b048b9031b9068c
zakypacartifa <kevinazzaky1@gmail.com>
2026-05-16 15:55:35 +0800

    feat: create responsive navbar

---
+++ src/constants/company.ts
@@ -0,0 +1,7 @@
+export const company = {
+  name: "Kaizen",
+  tagline: "Solusi Maintenance Equipment Kitchen Profesional",
+  whatsappNumber: "6281529500457",
+  whatsappMessage:
+    "Halo Kaizen, saya ingin konsultasi mengenai layanan maintenance kitchen equipment.",
+};
---
+++ src/constants/navigation.ts
@@ -0,0 +1,22 @@
+export const navigationItems = [
+  {
+    label: "Home",
+    href: "#home",
+  },
+  {
+    label: "Tentang Kami",
+    href: "#tentang-kami",
+  },
+  {
+    label: "Layanan",
+    href: "#layanan",
+  },
+  {
+    label: "Galeri",
+    href: "#galeri",
+  },
+  {
+    label: "Kontak",
+    href: "#kontak",
+  },
+];
